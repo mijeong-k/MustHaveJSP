1b5fe27e67996f83f8006202529268d8a6e6b1dc
Class01 <Class01@DESKTOP-1KRANK2>
2023-07-25 09:44:44 +0900

    JSP_~230725

--- MustHaveJSP/src/main/java/model2/mvcboard/MVCBoardDAO.java
+++ MustHaveJSP/src/main/java/model2/mvcboard/MVCBoardDAO.java
@@ -81,4 +81,137 @@ public class MVCBoardDAO extends DBConnPool{
 			return board;
 					
 		}
+		
+		public int insertWrite(MVCBoardDTO dto) {
+			int result=0;
+			try {
+				String query = "INSERT INTO mvcboard ( "
+						+ " idx, name, title, content, ofile, sfile, pass) "
+						+ " VALUES ( "
+						+ " seq_board_num.NEXTVAL,?,?,?,?,?,?)";
+				
+				psmt = con.prepareStatement(query);
+				psmt.setString(1, dto.getName());
+				psmt.setString(2, dto.getTitle());
+				psmt.setString(3, dto.getContent());
+				psmt.setString(4, dto.getOfile());
+				psmt.setString(5, dto.getSfile());
+				psmt.setString(6, dto.getPass());
+				result = psmt.executeUpdate();
+			}catch ( Exception e) {
+				System.out.println("게시물 입력 중 예외 발생");
+				e.printStackTrace();
+			}
+			return result;
+		}
+		
+		public MVCBoardDTO selectview(String idx) {
+			MVCBoardDTO dto = new MVCBoardDTO();
+			String query = "SELECT * FROM mvcboard WHERE idx=?";
+			try {
+				psmt=con.prepareStatement(query);
+				psmt.setString(1, idx);
+				rs=psmt.executeQuery();
+				
+				if(rs.next()) {
+					dto.setIdx(rs.getString(1));
+					dto.setName(rs.getString(2));
+					dto.setTitle(rs.getString(3));
+					dto.setContent(rs.getString(4));
+					dto.setPostdate(rs.getDate(5));
+					dto.setOfile(rs.getString(6));
+					dto.setSfile(rs.getString(7));
+					dto.setDowncount(rs.getInt(8));
+					dto.setPass(rs.getString(9));
+					dto.setVisitcount(rs.getInt(0));
+				}
+			}catch(Exception e) {
+				System.out.println("게시물 상세보기 중 예외 발생");
+				e.printStackTrace();
+			}
+			return dto;
+		}
+		
+		public void updateVisitCount(String idx) {
+			String query = "UPDATE mvcboard SET"
+						+ " visitcount=visitcount+1 "
+						+ " WHERE idx=?";
+			try {
+				psmt=con.prepareStatement(query);
+				psmt.setString(1, idx);
+				psmt.executeQuery();
+			}catch(Exception e) {
+				System.out.println("게시물 조회수 증가 중 예외 발생");
+				e.printStackTrace();
+			}
+		}
+		
+		public void downCountPlus(String idx) {
+			String sql="UPDATE mvcboard SET "
+					+ " downcount = downcount+1 "
+					+ " WHERE idx=? ";
+			try {
+				psmt=con.prepareStatement(sql);
+				psmt.setString(1, idx);
+				psmt.executeUpdate();
+			}catch (Exception e) {}
+		}
+		
+		public boolean confirmPassword(String pass, String idx) {
+			boolean isCorr = true;
+			try {
+				String sql="SELECT COUNT(*) FROM mvcboard WHERE pass=? AND idx=?";
+				psmt=con.prepareStatement(sql);
+				psmt.setString(1, pass);
+				psmt.setString(2, idx);
+				rs = psmt.executeQuery();
+				rs.next();
+				if(rs.getInt(1) == 0) {
+					isCorr = false;
+				}
+			}catch(Exception e) {
+				isCorr = false;
+				e.printStackTrace();
+			}
+			return isCorr;
+		}
+		
+		public int deletePost(String idx) {
+			int result = 0;
+			try {
+				String query = "DELETE FROM mvcboard WHERE idx=?";
+				psmt = con.prepareStatement(query);
+				psmt.setString(1, idx);
+				result = psmt.executeUpdate();
+			}catch( Exception e) {
+				System.out.println("게시물 삭제 중 예외 발생");
+				e.printStackTrace();
+			}
+			return result;
+			
+		}
+		
+		public int updatePost(MVCBoardDTO dto) {
+			int result = 0;
+			try {
+				String query = "UPDATE mvcboard"
+							+ " SET title=?, name=?, content=?, ofile=?, sfile=? "
+							+ " WHERE idx=? and pass=?";
+				
+				psmt=con.prepareStatement(query);
+				psmt.setString(1, dto.getTitle());
+				psmt.setString(2, dto.getName());
+				psmt.setString(3, dto.getContent());
+				psmt.setString(4, dto.getOfile());
+				psmt.setString(5, dto.getSfile());
+				psmt.setString(6, dto.getIdx());
+				psmt.setString(7, dto.getPass());
+				
+				result = psmt.executeUpdate();
+			}catch(Exception e) {
+				System.out.println("게시물 수정 중 예외 발생");
+				e.printStackTrace();
+			}
+			return result;
+		}
 }
--- MustHaveJSP/src/main/java/utils/JSFunction.java
+++ MustHaveJSP/src/main/java/utils/JSFunction.java
@@ -1,21 +1,50 @@
 package utils;
 
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.jsp.JspWriter;
 
 public class JSFunction {
 	public static void alertLocation(String msg, String url, JspWriter out) {
 		try {
-			String script = "" + "<script>" + "    alert('"+msg+"');"
-					+"    location.href='"+url+"';" + "</script>";
+			String script = "" + "<script>" + "    alert('" + msg + "');" + "    location.href='" + url + "';"
+					+ "</script>";
 			out.println(script);
-		}catch(Exception e) {}		
+		} catch (Exception e) {
+		}
 	}
-	
+
 	public static void alertBack(String msg, JspWriter out) {
 		try {
-			String script = "" + "<script>" + "    alert('"+msg+"');"
-					+"    history.back();" + "</script>";
+			String script = "" + "<script>" + "    alert('" + msg + "');" + "    history.back();" + "</script>";
 			out.println(script);
-		}catch(Exception e) {}			
+		} catch (Exception e) {
+		}
 	}
+
+	public static void alertLocation(HttpServletResponse resp, String msg, String url) {
+		try {
+			resp.setContentType("text/html;charset=UTF-8");
+			PrintWriter writer = resp.getWriter();
+			String script = "" + "<script>" + "alert('" + msg + "');" + "location.href='" + url + "';" + "</script>";
+			writer.print(script);
+		} catch (Exception e) {
+		}
+	}
+	
+	public static void alertBack(HttpServletResponse resp, String msg) {
+		try {
+			resp.setContentType("text/html;charset=UTF-8");
+			PrintWriter writer=resp.getWriter();
+			String script = ""
+					+ "<script>"
+					+" alert('"+msg+"');"
+					+" history.back();"
+					+ "</script>";
+			writer.print(script);
+					
+		}catch ( Exception e) {}
+	}
+
 }
